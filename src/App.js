37c4095cd598a744374dfe35f0d12c34bc418601
elham <elham.saboutii@gmail.com>
2023-08-06 11:55:09 +0330

    about route added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {Routes,Route} from "react-router-dom";
 import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
       </Routes>
     </div>
   );
--- src/Components/Layouts/Navigation.jsx
+++ src/Components/Layouts/Navigation.jsx
@@ -6,7 +6,7 @@ const Navigation = () => {
         <nav className="h-[41px] w-[700px] bg-rose-800 text-white">
             <ul>
                 <li><Link to="/">صفحه اصلی</Link></li>
-                <li><Link to="#">سبد خرید</Link></li>
+                <li><Link to="/about">سبد خرید</Link></li>
                 <li><Link to="#">محصولات</Link></li>
                 <li><Link to="#">درباره ما</Link></li>
                 <li><Link to="#">سوالات متداول</Link></li>
