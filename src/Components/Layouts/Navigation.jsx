1d05df573f6d232e3cd75107098fe33818251c82
elham <elham.saboutii@gmail.com>
2023-08-08 19:49:23 +0330

    data added & functionality navigation

--- src/Components/Layouts/Navigation.jsx
+++ src/Components/Layouts/Navigation.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 const Navigation = () => {
+    const checkActive = ({ isActive }) => {return isActive?"px-1 py-2.5 border-b border-b-[#01C38E] text-[#01C38E]":"px-1 py-2.5"}
     return (
         <nav className="h-[41px] w-[650px]  text-white ml-[75px]">
             <ul className="flex flex items-center justify-between flex-row-reverse w-full h-full text-[16px] ">
-                <li><Link className="px-1 text-[#01C38E] border-b border-b-[#01C38E] py-2.5" to="/">صفحه اصلی</Link></li>
-                <li><Link className="px-1 py-2.5" to="/about">سبد خرید</Link></li>
-                <li><Link className="px1 py-2.5" to="#">محصولات</Link></li>
-                <li><Link className="px-1 py-2.5" to="#">درباره ما</Link></li>
-                <li><Link className="px1 py-2.5" to="#">سوالات متداول</Link></li>
+                <li><NavLink className={(data)=>checkActive(data) } to="/" >صفحه اصلی</NavLink></li>
+                <li><NavLink className={(data)=>checkActive(data)} to="/about">سبد خرید</NavLink></li>
+                <li><NavLink className={(data)=>checkActive(data)} to="/about">محصولات</NavLink></li>
+                <li><NavLink className={(data)=>checkActive(data)} to="/about">درباره ما</NavLink></li>
+                <li><NavLink className={(data)=>checkActive(data)} to="/about">سوالات متداول</NavLink></li>
             </ul>
         </nav>
     );
