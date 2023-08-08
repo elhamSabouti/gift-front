cb2a85529c42d455fe66ee0758bc38a87391e95e
elham <elham.saboutii@gmail.com>
2023-08-08 18:09:06 +0330

    dots box added to home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Header from "../Layouts/Header";
 import Slider from "react-slick";
-import Banners from "./Banners/Banners";
-import Products from "./Products/Products";
+import Adds from "./Adds/Adds";
+import Categories from "./Categories/Categories";
+import Products from "../Products/Products";
+import Dots from "./Dots";
 
 const Home = () => {
     return (
-        <div className="w-full h-[100vh] bg-[#1A1E29] flex items-center justify-start flex-col">
+        <div className="w-full min-h-[100vh] bg-[#1A1E29] flex items-center justify-start flex-col">
             <Header/>
-            <Banners/>
+            <Adds/>
+            <Categories/>
             <Products/>
-
+            <Dots/>
         </div>
     );
 };
