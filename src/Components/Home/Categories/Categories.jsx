6f99ea7e2bf4796dcce7982418ac0e9f87811a8b
elham <elham.saboutii@gmail.com>
2023-08-13 18:32:59 +0330

    categories data changed

--- src/Components/Home/Categories/Categories.jsx
+++ src/Components/Home/Categories/Categories.jsx
@@ -1,34 +1,29 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import CategoryBtn from "./CategoryBtn";
+import API from "../../../Api/useApi";
 
 const Categories = () => {
-    const data=[
-        {
-            "title": "همه",
-            "link": "http://prohaska.biz/est-distinctio-occaecati-sed-animi-totam-dolor",
-         },
-        {
-            "title": "موزیک",
-            "link": "http://prohaska.biz/est-distinctio-occaecati-sed-animi-totam-dolor",
-   },
-        {
-            "title": "فیلم و سریال",
-            "link": "http://prohaska.biz/est-distinctio-occaecati-sed-animi-totam-dolor",
-      },
-        {
-            "title": "آموزشی",
-            "link": "http://prohaska.biz/est-distinctio-occaecati-sed-animi-totam-dolor",
-       },
-        {
-            "title": "ابزار",
-            "link": "http://prohaska.biz/est-distinctio-occaecati-sed-animi-totam-dolor",
-    }];
+    const [categories,setCategories]=useState(null);
+    const [activeCategoryId,setActiveCategoryId]=useState(0);
+    const  getCategories =async () => {
+        try {
+            const category= await API.get("category");
+            setCategories(category.data.data)
+        }catch (e) {
+        }
+    }
+    useEffect(()=>{
+        getCategories();
+    },[]);
     return (
         <div  className="flex-col items-between justify-center flex-wrap h-auto  mt-[16px] max-w-[89%]">
             <p className="text-[#01C38E] text-[34px] h-[53px] ">محصولات</p>
             <ul className="flex w-auto flex-wrap justify-center items-center gap-[22px] h-[48px] mt-[12px] flex-row-reverse">
-                {data.map((value,index)=>(
-                        <CategoryBtn props={value} id={index}/>
+                <li>
+                    <button className={`w-[170px] h-[47px]  flex justify-center items-center ${activeCategoryId===0?`bg-[#132D46] border-2 border-[#01C38E] text-white `:`bg-white text-[#1A1E29]`} rounded-[20px] text-[20px] `} onClick={()=>setActiveCategoryId(0)}>همه</button>
+                </li>
+                {categories && categories.map((value,k)=>(
+                        <CategoryBtn props={value} key={k} setActiveCategoryId={setActiveCategoryId} activeCategoryId={activeCategoryId}/>
                     ))}
             </ul>
         </div>
--- src/Components/Home/Categories/CategoryBtn.jsx
+++ src/Components/Home/Categories/CategoryBtn.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const CategoryBtn = ({props,id}) => {
+const CategoryBtn = ({props,setActiveCategoryId,activeCategoryId}) => {
     return (
-        <li className="" key={id}>
-            <button className={`w-[170px] h-[47px]  flex justify-center items-center ${id===0 ?`bg-[#132D46] border-2 border-[#01C38E] text-white`:`bg-white text-[#1A1E29]`} rounded-[20px] text-[20px] ;`}>{props.title}</button>
+        <li>
+            <button onClick={()=>setActiveCategoryId(props.id)} className={`w-[170px] h-[47px]  flex justify-center items-center  rounded-[20px] text-[20px] ${activeCategoryId===props.id?`bg-[#132D46] border-2 border-[#01C38E] text-white `:`bg-white text-[#1A1E29]`} ` } >{props.title}</button>
         </li>
     );
 };
